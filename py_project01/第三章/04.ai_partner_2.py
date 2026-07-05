008c82aee5c996f0d1f3d920fa240f360146779d
13854048335 <1678602724@qq.com>
2026-07-05 10:07:27 +0800

    2026/07/03

---
+++ py_project01/第三章/04.ai_partner_2.py
@@ -0,0 +1,63 @@
+import os
+import streamlit as st
+from openai import OpenAI
+# 设置页面的配置项
+st.set_page_config(
+    page_title="AI智能伴侣",
+    page_icon="🤖",
+    # 布局
+    layout="wide",
+    # 控制的是侧边栏的状态
+    initial_sidebar_state="expanded",
+    menu_items={}
+)
+
+# 大标题
+st.title("AI智能伴侣")
+#Logo
+st.logo("./resources/logo.png")
+
+systme_prompt = "你是一个AI助理，请使用正式的语气回答客户的问题"
+# 初始化聊天信息
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+# 展示聊天信息
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).write(message["content"])
+
+
+# 输入框
+prompt = st.chat_input("请输入你要问的问题：")
+if prompt:  # 字符串自动转换为布尔值，如果字符串非空，则返回True
+    st.chat_message("user").write(prompt)
+    print(f"调用AI大模型，提示词：{prompt}")
+    # 保存用户输入
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # 调用AI大模型
+    # 创建与AI大模型交互的客户端
+    client = OpenAI(
+        api_key=os.environ.get('DEEPSEEK_API_KEY'),
+        base_url="https://api.deepseek.com")
+    # 与AI大模型进行交互
+    response = client.chat.completions.create(
+        model="deepseek-v4-pro",
+        messages=[
+            {"role": "system", "content": systme_prompt},
+            *st.session_state.messages
+        ],
+        stream=True,
+    )
+    # 非流式输出解析方式
+    # print("--------->大模型返回的结果",response.choices[0].message.content)
+    # st.chat_message("assistant").write(response.choices[0].message.content)
+    # 流式输出解析方式
+    response_message = st.empty() # 创建一个空的组件，用于展示大模型返回的结果
+    full_response = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            content = chunk.choices[0].delta.content
+            full_response += content
+            response_message.chat_message("assistant").write(full_response)
+    # 保存大模型返回的结果
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
